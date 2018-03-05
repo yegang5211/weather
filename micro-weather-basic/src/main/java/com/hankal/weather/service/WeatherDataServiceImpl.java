c38e968ca38bc960374d89ec5a416bc3373700c1
yegang5211 <yegang5211@163.com>
2018-03-06 07:56:27 +0800

    添加了redis功能，天气信息从网上抓取后放到redis里

--- micro-weather-basic/src/main/java/com/hankal/weather/service/WeatherDataServiceImpl.java
+++ micro-weather-basic/src/main/java/com/hankal/weather/service/WeatherDataServiceImpl.java
@@ -1,56 +1,78 @@
 package com.hankal.weather.service;
 
-import java.io.IOException;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hankal.weather.redis.RedisService;
+import com.hankal.weather.redis.WeatherDataKey;
 import com.hankal.weather.vo.WeatherResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 
 /**
  * WeatherDataService 实现.
- *
  */
 @Service
 public class WeatherDataServiceImpl implements WeatherDataService {
-	private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
-	
-	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Override
-	public WeatherResponse getDataByCityId(String cityId) {
-		String uri = WEATHER_URI + "citykey=" + cityId;
-		return this.doGetWeahter(uri);
-	}
+    private final static Logger logger = LoggerFactory.getLogger(WeatherDataServiceImpl.class);
 
-	@Override
-	public WeatherResponse getDataByCityName(String cityName) {
-		String uri = WEATHER_URI + "city=" + cityName;
-		return this.doGetWeahter(uri);
-	}
-	
-	private WeatherResponse doGetWeahter(String uri) {
- 		ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
-		
-		ObjectMapper mapper = new ObjectMapper();
-		WeatherResponse resp = null;
-		String strBody = null;
-		
-		if (respString.getStatusCodeValue() == 200) {
-			strBody = respString.getBody();
-		}
+    private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
 
-		try {
-			resp = mapper.readValue(strBody, WeatherResponse.class);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return resp;
-	}
+    private static final long TIME_OUT = 1800L; // 1800s
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    RedisService redisService;
+
+    @Override
+    public WeatherResponse getDataByCityId(String cityId) {
+        String uri = WEATHER_URI + "citykey=" + cityId;
+        return this.doGetWeahter(uri);
+    }
+
+    @Override
+    public WeatherResponse getDataByCityName(String cityName) {
+        String uri = WEATHER_URI + "city=" + cityName;
+        return this.doGetWeahter(uri);
+    }
+
+    private WeatherResponse doGetWeahter(String uri) {
+
+        String key = uri;
+        String strBody = null;
+        ObjectMapper mapper = new ObjectMapper();
+        WeatherResponse resp = null;
+
+        // 先查缓存，缓存有的取缓存中的数据
+        if (redisService.exists(WeatherDataKey.getById, "" + uri, String.class)) {
+            logger.info("Redis has data");
+            strBody = redisService.get(WeatherDataKey.getById, "" + uri, String.class);
+        } else {
+            logger.info("Redis don't has data");
+            // 缓存没有，再调用服务接口来获取
+            ResponseEntity<String> respString = restTemplate.getForEntity(uri, String.class);
+
+            if (respString.getStatusCodeValue() == 200) {
+                strBody = respString.getBody();
+            }
+
+            // 数据写入缓存
+            redisService.set(WeatherDataKey.getById, "" + uri, strBody);
+        }
+
+        try {
+            resp = mapper.readValue(strBody, WeatherResponse.class);
+        } catch (IOException e) {
+            //e.printStackTrace();
+            logger.error("Error!", e);
+        }
+
+        return resp;
+    }
 }
